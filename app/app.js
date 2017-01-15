23d2cc19fe8982ef0bd4b54aeacb5722e44872af
evan <evanfungv@gmail.com>
2017-01-15 17:04:49 +0800

    using express

--- app/app.js
+++ app/app.js
@@ -1,12 +1,22 @@
-var http = require('http');
+// var http = require('http');
+//
+// var myServer = http.createServer(function (request,response) {
+//   response.writeHead(200,{"Content-Type":"text/html"});
+//
+//   response.write('<h1>Roux Meetups</h1>');
+//
+//   response.end();
+// });
+//
+// myServer.listen(3000);
+// console.log('Go to localhost 3000 on your browser');
 
-var myServer = http.createServer(function (request,response) {
-  response.writeHead(200,{"Content-Type":"text/html"});
-
-  response.write('<h1>Roux Meetups</h1>');
-
-  response.end();
+var express = require('express');
+var app = express();
+app.get('/',function(req,res){
+  res.send('<h1>Roux Academy Meetups</h1>');
 });
 
-myServer.listen(3000);
-console.log('Go to localhost 3000 on your browser');
+var server = app.listen(3000,function() {
+  console.log('Listen 3000');
+})
