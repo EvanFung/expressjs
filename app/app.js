c9e17ec57aca9d27eb6298186c8bbeaaf773601e
evan <evanfungv@gmail.com>
2017-01-15 21:27:41 +0800

    some changed

--- app/app.js
+++ app/app.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 var dataFile = require('./data/data.json');
+
 app.set('port',process.env.PORT || 3000);
 app.set('appData',dataFile);
+
+app.use(express.static('app/public'));
 app.use(require('./routes/index'));
-app.use(require('./routes/speakers'))
+app.use(require('./routes/speakers'));
+
 var server = app.listen(app.get('port'),function() {
   console.log('Listen on port ' + app.get('port'));
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 router.get('/',function(req,res){
   res.send(`
     <h1>Welcome</h1>
+    <img src="/images/misc/background.jpg" alt="background" style="height:300px;"/>
     `);
 
 });
