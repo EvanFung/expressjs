11ea09365e0f1c723c8a22162783834d11d301fe
evan <evanfungv@gmail.com>
2017-01-15 17:45:20 +0800

    basic routing

--- app/app.js
+++ app/app.js
@@ -1,22 +1,24 @@
-// var http = require('http');
-//
-// var myServer = http.createServer(function (request,response) {
-//   response.writeHead(200,{"Content-Type":"text/html"});
-//
-//   response.write('<h1>Roux Meetups</h1>');
-//
-//   response.end();
-// });
-//
-// myServer.listen(3000);
-// console.log('Go to localhost 3000 on your browser');
-
 var express = require('express');
 var app = express();
+var dataFile = require('./data/data.json');
+app.set('port',process.env.PORT || 3000);
 app.get('/',function(req,res){
-  res.send('<h1>Roux Academy Meetups</h1>');
+  var info = '';
+  dataFile.speakers.forEach(function (item) {
+    info += `
+    <li>
+      <h2>${item.name}</h2>
+      <p>${item.summary}</p>
+    </li>
+    `;
+  });
+  res.send(`
+    <h1>Roux Academy Meetups</h1>
+    ${info}
+    `);
+
 });
 
-var server = app.listen(3000,function() {
-  console.log('Listen 3000');
-})
+var server = app.listen(app.get('port'),function() {
+  console.log('Listen on port ' + app.get('port'));
+});
