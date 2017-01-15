65a722f482284ea705acfc82807b67671c743de8
evan <evanfungv@gmail.com>
2017-01-15 17:53:03 +0800

    basic routing 2

--- app/app.js
+++ app/app.js
@@ -3,6 +3,13 @@ var app = express();
 var dataFile = require('./data/data.json');
 app.set('port',process.env.PORT || 3000);
 app.get('/',function(req,res){
+  res.send(`
+    <h1>Welcome</h1>
+    `);
+
+});
+
+app.get('/speakers',function(req,res){
   var info = '';
   dataFile.speakers.forEach(function (item) {
     info += `
@@ -15,6 +22,17 @@ app.get('/',function(req,res){
   res.send(`
     <h1>Roux Academy Meetups</h1>
     ${info}
+    `);
+
+});
+
+
+app.get('/speakers/:speakerid',function(req,res){
+  var speaker = dataFile.speakers[req.params.speakerid];
+  res.send(`
+    <h1>${speaker.title}</h1>
+    <h2>with${speaker.name}</h2>
+    <p>${speaker.summary}</p>
     `);
 
 });
